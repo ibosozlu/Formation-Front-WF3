82ad783611b8847697c8e515243f048a6992930d
Ibo SOZLU <39770074+ibosozlu@users.noreply.github.com>
2018-06-11 17:01:04 +0200

    Travail du 11/06
    Fin de JQUERY

--- Front/Jquery/assets/js/08.contact.js
+++ Front/Jquery/assets/js/08.contact.js
@@ -18,8 +18,22 @@ $(function() {
     function afficheUneNotification() {}
 
     // -- Vérification de la présence d'un Contact dans Contacts
-    function estCeQunContactEstPresent(UnEmail) {}
+    function estCeQunContactEstPresent(UnEmail) {
 
+            // -- Booleen qui indique la présence d'un contact dans ma collection
+            let estPresent = false;
+
+            // -- On parcourt le tableau à la recherche d'une correspondance
+            for(let i = 0; i < CollectionDeContacts.length ; i++){
+
+                if (UnEmail === CollectionDeContacts[i].email){
+                    estPresent = true;
+                    break;
+                }
+
+            }
+            return estPresent
+    }
     // -- Vérification de la validité d'un Email
     // : https://paulund.co.uk/regular-expression-to-validate-email-address
     function validateEmail(email){}
@@ -29,43 +43,77 @@ $(function() {
     -------------------------------------------------------------- */
 
     // -- Détection de la soumission de mon Formulaire
-    $('#contact').on('submit', function(e) {
+        $('#contact').on('submit', function(e) {
+        l = e => console.log(e)
 
         // -- Voir le contenu de l'évènement
+        l(e);
 
         // -- Stopper la redirection de la page
+        event.preventDefault();
 
         // -- Récupération des champs à vérifier
+        const prenom,nom,email,tel;
+        nom = $('#nom');
+        prenom = $('#prenom');
+        email = $('#email');
+        tel = $('#tel');
+
 
         // -- Vérification des informations...
-        
+        let mesInformationsSontValides = true;
+
         // -- Vérification du Prénom
-
+        if (prenom.val().length === 0){
+            // -- Le champ est incorrect, car il n'a pas été rempli ...
+            mesInformationsSontValides = false;
+        }
         // -- Vérification du Nom
-
+        if (nom.val().length === 0){
+            mesInformationsSontValides = false;
+        }
         // -- Vérification du Tel
+        let tel_regex = /^(\+33 |0)[1-9 ]( \d\d){4}$/
 
+        if(!tel_regex.test(tel.val())){
+            mesInformationsSontValides = false;
+        }
         // -- Vérification du Mail
+        let email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-        if(...) {
+        if(!email_regex.test(email.val())){
+            mesInformationsSontValides = false; 
+        }
+
+
+        if(mesInformationsSontValides) {
             
             // -- Tous est correct, Préparation du Contact
+            let Contact = {
+                // cle     // valeur
+                nom         : nom.val(),
+                prenom      : prenom.val(),
+                email       : email.val(),
+                tel         : tel.val(),
 
+            }
             // -- Observons dans la console
             console.log(Contact);
 
             // -- Vérification avec EstCeQunContactEstPresent
-            if(!estCeQunContactEstPresent(...) {
+            if(!estCeQunContactEstPresent(Contact.email)) {   
                 
                 // -- Ajout du Contact
                 ajouterContact(Contact);
 
             } else {                    
                 // -- Alert si contact présent
+                alert('ATTENTION\nCE CONTACT EST DEJA PRESENT');
             }
 
         } else {
             // -- Alert si erreur dans l'un des champs
+            alert('ATTENTION\nVeuillez bien remplir tous les champs');
         }       
 
     });
